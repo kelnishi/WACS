7a482bde8e93ccf3c1a9ef70fbcd1b0cd4b31c99
Kelvin Nishikawa <kelnishi@gmail.com>
2024-10-18 01:35:48 -0700

    Simple test Hello.wasm!

--- Wacs.Console/Program.cs
+++ Wacs.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using CommandLine;
 using FluentValidation;
 using Wacs.Core;
+using Wacs.Core.Runtime;
 
 
 namespace Wacs.Console
@@ -44,6 +45,32 @@ namespace Wacs.Console
                             break;
                     }
                 }
+
+                var runtime = new WasmRuntime();
+                runtime.BindHostFunction("env","sayc", (scalars) =>
+                {
+                    char c = Convert.ToChar(scalars[0]);
+                    System.Console.Write(c);
+                    return Array.Empty<object>();
+                });
+                
+                var modInst = runtime.InstantiateModule(module, new(){SkipModuleValidation = true});
+                runtime.RegisterModule("hello", modInst);
+
+                var mainAddr = runtime.GetExportedFunction("hello", "main");
+                if (mainAddr != null)
+                {
+                    runtime.Invoke(mainAddr);
+                    int steps = 0;
+                    while (runtime.ProcessThread())
+                    {
+                        steps += 1;
+                    }
+                    System.Console.WriteLine($"Process used {steps} gas.");
+                }
+                
+                
+                System.Console.WriteLine(modInst);
             }
         }
     }
