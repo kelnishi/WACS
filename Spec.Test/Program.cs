e347abd781331f2a577824af25f4e5a372aac35d
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-04 13:49:05 -0800

    Add 'spec' submodule with sparse checkout of 'test' directory

---
+++ Spec.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spec.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
