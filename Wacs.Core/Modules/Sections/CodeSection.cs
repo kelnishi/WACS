4443b23d5fd371ea2efdad75a5d4f9f0c87b5a75
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-05 17:25:50 -0800

    Fixing spec test failures.

--- Wacs.Core/Modules/Sections/CodeSection.cs
+++ Wacs.Core/Modules/Sections/CodeSection.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Wacs.Core.Types;
@@ -9,6 +8,8 @@ namespace Wacs.Core
 {
     public partial class Module
     {
+        public CodeDesc[] Codes { get; internal set; } = Array.Empty<CodeDesc>();
+
         /// <summary>
         /// @Spec 2.5.3 Functions
         /// </summary>
@@ -20,13 +21,19 @@ namespace Wacs.Core
             private (uint Number, ValType Type)[] CompressedLocals { get; }
             public Expression Body { get; }
 
+            public long NumberOfLocals => CompressedLocals.Sum(t => t.Number);
+
             public ValType[] Locals => 
                 CompressedLocals
                     .SelectMany(local => Enumerable.Repeat(local.Type, (int)local.Number))
                     .ToArray();
 
-            private static (uint Number, ValType Type) ParseCompressedLocal(BinaryReader reader) =>
-                (reader.ReadLeb128_u32(), ValTypeParser.Parse(reader));
+            private static (uint Number, ValType Type) ParseCompressedLocal(BinaryReader reader)
+            {
+                uint count = reader.ReadLeb128_u32();
+                var expr = ValTypeParser.Parse(reader);
+                return (count, expr);
+            }
 
             public static FuncLocalsBody Parse(BinaryReader reader) =>
                 new(reader.ParseVector(ParseCompressedLocal), Expression.Parse(reader));
@@ -53,6 +60,8 @@ namespace Wacs.Core
     
     public static partial class BinaryModuleParser
     {
+        public static uint MaximumFunctionLocals = 1024;
+
         /// <summary>
         /// @Spec 5.5.13. Code Section
         /// </summary>
@@ -62,17 +71,22 @@ namespace Wacs.Core
         /// <summary>
         /// @Spec 5.5.13. Code Section
         /// </summary>
-        private static void PatchFuncSection(List<Module.Function> functions, Module.CodeDesc[] code)
+        private static void PatchFuncSection(Module module)
         {
-            if (functions.Count != code.Length)
-                throw new FormatException($"Module functions section count {functions.Count} must equal the code count {code.Length}");
+            if (module.Funcs.Count != module.Codes.Length)
+                throw new FormatException($"Module functions section count {module.Funcs.Count} must equal the code count {module.Codes.Length}");
             
-            for (int i = 0, l = code.Length; i < l; ++i)
+            for (int i = 0, l = module.Codes.Length; i < l; ++i)
             {
-                var localsbody = code[i].Code;
-                functions[i].Locals = localsbody.Locals;
-                functions[i].Body = localsbody.Body;
+                var localsbody = module.Codes[i].Code;
+                if (localsbody.NumberOfLocals > MaximumFunctionLocals)
+                    throw new FormatException(
+                        $"Function[{i}] locals count {localsbody.NumberOfLocals} exceeds maximum allowed {MaximumFunctionLocals}");
+                module.Funcs[i].Locals = localsbody.Locals;
+                module.Funcs[i].Body = localsbody.Body;
             }
+
+            module.Codes = Array.Empty<Module.CodeDesc>();
         }
     }    
 }
