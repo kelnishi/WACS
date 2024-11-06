8419a6a136fba6ea31538d51e6e83ad672323c14
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-05 16:27:02 -0800

    Fixing LEB128 signed decoding

--- Wacs.Core/Utilities/BinaryReaderExtension.cs
+++ Wacs.Core/Utilities/BinaryReaderExtension.cs
@@ -18,9 +18,11 @@ namespace Wacs.Core.Utilities
             do
             {
                 byteValue = reader.ReadByte();
+                result |= (uint)(byteValue & 0x7F) << shift;
+                
                 if (shift == 28 && (byteValue & 0x70) != 0)
                     throw new FormatException("LEB128_s32 had too many bits");
-                result |= (uint)(byteValue & 0x7F) << shift;
+                
                 shift += 7;
             } while ((byteValue & 0x80) != 0);
 
@@ -39,15 +41,28 @@ namespace Wacs.Core.Utilities
             do
             {
                 byteValue = reader.ReadByte();
-                if (shift == 28 && (byteValue & 0x40) == 0 && (byteValue & 0x30) != 0)
-                    throw new FormatException("LEB128_s32 had too many bits");
                 result |= (byteValue & 0x7F) << shift; //low-order 7 bits
+
+                if (shift > 32)
+                {
+                    throw new FormatException($"LEB128_s32 had too many bytes");
+                }
+                if (shift > 32 - 8)
+                {
+                    int extraBits = (0xFF << (32 - shift)) & 0x7F;
+                    int checkBits = byteValue & extraBits;
+                    switch (result >= 0)
+                    {
+                        case true when checkBits != 0: 
+                            throw new FormatException("$LEB128_s32 had overflow bits");
+                        case false when checkBits != extraBits:
+                            throw new FormatException($"LEB128_s32 did not have overflow bits for negative");
+                    }
+                }
+
                 shift += 7;
             } while ((byteValue & 0x80) != 0); //high-order bit != 0
 
-            if (shift > 35)
-                throw new FormatException("LEB128_s32 had too many bytes");
-            
             // If the sign bit of the last byte read is set, extend the sign
             if (shift < 32 && (byteValue & 0x40) != 0)
             {
@@ -62,34 +77,39 @@ namespace Wacs.Core.Utilities
         {
             long result = 0;
             int shift = 0;
+            byte byteValue;
 
-            while (true)
+            do
             {
-                int byteValue = reader.ReadByte();
-                if (byteValue == -1)
-                    throw new FormatException("Unexpected end of stream while decoding s64.");
+                byteValue = reader.ReadByte();
+                result |= (byteValue & 0x7FL) << shift; //low-order 7 bits
 
-                byte lower7Bits = (byte)(byteValue & 0x7F);
-                result |= (long)lower7Bits << shift;
-
-                shift += 7;
-
-                bool moreBytes = (byteValue & 0x80) != 0;
-
-                if (!moreBytes)
+                if (shift > 64)
                 {
-                    if ((byteValue & 0x40) != 0 && shift < 65)
+                    throw new FormatException($"LEB128_s{64} had too many bytes");
+                }
+                if (shift > 64 - 8)
+                {
+                    int extraBits = (0xFF << (64 - shift)) & 0x7F;
+                    int checkBits = byteValue & extraBits;
+                    switch (result >= 0)
                     {
-                        result |= -1L << shift;
+                        case true when checkBits != 0: 
+                            throw new FormatException("$LEB128_s{width} had overflow bits");
+                        case false when checkBits != extraBits:
+                            throw new FormatException($"LEB128_s{64} did not have overflow bits for negative");
                     }
-
-                    break;
                 }
 
-                if (shift >= 64)
-                    throw new FormatException("Shift count exceeds 64 bits while decoding s64.");
-            }
+                shift += 7;
+            } while ((byteValue & 0x80) != 0); //high-order bit != 0
 
+            // If the sign bit of the last byte read is set, extend the sign
+            if (shift < 64 && (byteValue & 0x40) != 0)
+            {
+                // Sign extend the result
+                result |= -(1L << shift); //~0 << shift;
+            }
 
             return result;
         }
