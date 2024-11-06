4d79360f20f26056a573d70b8a65addb0b66f963
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-06 14:17:13 -0800

    Fixing spec test ref types

--- Spec.Test/WastJson/Argument.cs
+++ Spec.Test/WastJson/Argument.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text.Json.Serialization;
 using Wacs.Core.Runtime;
+using Wacs.Core.Types;
 
 namespace Spec.Test.WastJson
 {
@@ -19,6 +20,8 @@ namespace Spec.Test.WastJson
                 "i64" => new Value(ulong.Parse(Value)),
                 "f32" => new Value(BitBashFloat(Value)),
                 "f64" => new Value(BitBashDouble(Value)),
+                "funcref" => new Value(ValType.Funcref, int.Parse(Value)),
+                "externref" => new Value(ValType.Externref, int.Parse(Value)),
                 _ => throw new ArgumentException($"Cannot parse value {Value} of type {Type}")
             };
 
