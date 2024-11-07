0af982cba6e2bc9ac862d5b3bb061a22d85a282e
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-06 19:15:35 -0800

    Spec.Test adding exhaustion test

--- Spec.Test/WastJson/IAction.cs
+++ Spec.Test/WastJson/IAction.cs
@@ -1,17 +1,13 @@
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace Spec.Test.WastJson
 {
     public interface IAction
     {
-        [JsonPropertyName("type")]
         ActionType Type { get; }
 
-        [JsonPropertyName("field")]
         string Field { get; set; }
 
-        [JsonPropertyName("args")]
         List<Argument> Args { get; set; }
     }
 }
