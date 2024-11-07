b33c1dc11c59fd66b36674e02dc83dc1376e006d
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-06 22:49:27 -0800

    Adding manually bound global vars

--- Spec.Test/SpecTestEnv.cs
+++ Spec.Test/SpecTestEnv.cs
@@ -1,5 +1,6 @@
 using System;
 using Wacs.Core.Runtime;
+using Wacs.Core.Types;
 
 namespace Spec.Test
 {
@@ -9,6 +10,15 @@ namespace Spec.Test
         {
             string module = "spectest";
             runtime.BindHostFunction<Action<int>>((module, "print_i32"), Print_I32);
+            runtime.BindHostMemory((module, "memory"), new MemoryType(1,2));
+            runtime.BindHostGlobal((module, "global_i32"), new GlobalType(ValType.I32, Mutability.Immutable),
+                new Value(ValType.I32, 666));
+            runtime.BindHostGlobal((module, "global_i64"), new GlobalType(ValType.I64, Mutability.Immutable),
+                new Value(ValType.I64, 666L));
+            runtime.BindHostGlobal((module, "global_f32"), new GlobalType(ValType.F32, Mutability.Immutable),
+                new Value(ValType.F32, 666.6f));
+            runtime.BindHostGlobal((module, "global_f64"), new GlobalType(ValType.F64, Mutability.Immutable),
+                new Value(ValType.F64, 666.6));
         }
 
         public void Print_I32(int value)
