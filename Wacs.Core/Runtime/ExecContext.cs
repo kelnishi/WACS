c3a5dfb2f26855c4b7289d6c89ee0b4f3abfd503
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-11 21:40:51 -0800

    Applying template header

--- Wacs.Core/Runtime/ExecContext.cs
+++ Wacs.Core/Runtime/ExecContext.cs
@@ -1,3 +1,19 @@
+// /*
+//  * Copyright 2024 Kelvin Nishikawa
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -31,13 +47,12 @@ namespace Wacs.Core.Runtime
 
     public class ExecContext
     {
+        private readonly InstructionSequence _hostReturnSequence;
         public readonly RuntimeAttributes Attributes;
 
         private Stack<Value> _asideVals = new();
 
         private InstructionSequence _currentSequence;
-
-        private InstructionSequence _hostReturnSequence;
         private int _sequenceIndex;
 
         public Dictionary<ushort, ExecStat> Stats = new();
