d1027b1d705a920bcfb616ff975a9ee198c8f4b0
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-11 16:42:01 -0800

    Fixing spec test simd visatbinops

--- Wacs.Core/Instructions/SIMD/ViSatBinOp.cs
+++ Wacs.Core/Instructions/SIMD/ViSatBinOp.cs
@@ -24,22 +24,22 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (sbyte)Math.Clamp((int)v1.U8x16_0 + (int)v2.U8x16_0, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_1 + (int)v2.U8x16_1, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_2 + (int)v2.U8x16_2, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_3 + (int)v2.U8x16_3, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_4 + (int)v2.U8x16_4, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_5 + (int)v2.U8x16_5, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_6 + (int)v2.U8x16_6, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_7 + (int)v2.U8x16_7, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_8 + (int)v2.U8x16_8, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_9 + (int)v2.U8x16_9, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_A + (int)v2.U8x16_A, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_B + (int)v2.U8x16_B, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_C + (int)v2.U8x16_C, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_D + (int)v2.U8x16_D, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_E + (int)v2.U8x16_E, sbyte.MinValue, sbyte.MaxValue),
-                (sbyte)Math.Clamp((int)v1.U8x16_F + (int)v2.U8x16_F, sbyte.MinValue, sbyte.MaxValue)
+                (sbyte)Math.Clamp((int)v1.I8x16_0 + (int)v2.I8x16_0, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_1 + (int)v2.I8x16_1, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_2 + (int)v2.I8x16_2, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_3 + (int)v2.I8x16_3, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_4 + (int)v2.I8x16_4, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_5 + (int)v2.I8x16_5, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_6 + (int)v2.I8x16_6, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_7 + (int)v2.I8x16_7, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_8 + (int)v2.I8x16_8, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_9 + (int)v2.I8x16_9, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_A + (int)v2.I8x16_A, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_B + (int)v2.I8x16_B, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_C + (int)v2.I8x16_C, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_D + (int)v2.I8x16_D, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_E + (int)v2.I8x16_E, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_F + (int)v2.I8x16_F, sbyte.MinValue, sbyte.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
@@ -74,14 +74,14 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (short)Math.Clamp((int)v1.U16x8_0 + (int)v2.U16x8_0, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_1 + (int)v2.U16x8_1, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_2 + (int)v2.U16x8_2, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_3 + (int)v2.U16x8_3, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_4 + (int)v2.U16x8_4, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_5 + (int)v2.U16x8_5, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_6 + (int)v2.U16x8_6, short.MinValue, short.MaxValue),
-                (short)Math.Clamp((int)v1.U16x8_7 + (int)v2.U16x8_7, short.MinValue, short.MaxValue)
+                (short)Math.Clamp((int)v1.I16x8_0 + (int)v2.I16x8_0, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_1 + (int)v2.I16x8_1, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_2 + (int)v2.I16x8_2, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_3 + (int)v2.I16x8_3, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_4 + (int)v2.I16x8_4, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_5 + (int)v2.I16x8_5, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_6 + (int)v2.I16x8_6, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_7 + (int)v2.I16x8_7, short.MinValue, short.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
@@ -109,22 +109,22 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_0 - v2.U8x16_0)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_1 - v2.U8x16_1)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_2 - v2.U8x16_2)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_3 - v2.U8x16_3)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_4 - v2.U8x16_4)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_5 - v2.U8x16_5)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_6 - v2.U8x16_6)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_7 - v2.U8x16_7)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_8 - v2.U8x16_8)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_9 - v2.U8x16_9)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_A - v2.U8x16_A)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_B - v2.U8x16_B)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_C - v2.U8x16_C)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_D - v2.U8x16_D)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_E - v2.U8x16_E)),
-                (sbyte)Math.Min(sbyte.MaxValue, Math.Max(sbyte.MinValue, v1.U8x16_F - v2.U8x16_F))
+                (sbyte)Math.Clamp((int)v1.I8x16_0 - (int)v2.I8x16_0, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_1 - (int)v2.I8x16_1, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_2 - (int)v2.I8x16_2, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_3 - (int)v2.I8x16_3, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_4 - (int)v2.I8x16_4, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_5 - (int)v2.I8x16_5, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_6 - (int)v2.I8x16_6, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_7 - (int)v2.I8x16_7, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_8 - (int)v2.I8x16_8, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_9 - (int)v2.I8x16_9, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_A - (int)v2.I8x16_A, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_B - (int)v2.I8x16_B, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_C - (int)v2.I8x16_C, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_D - (int)v2.I8x16_D, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_E - (int)v2.I8x16_E, sbyte.MinValue, sbyte.MaxValue),
+                (sbyte)Math.Clamp((int)v1.I8x16_F - (int)v2.I8x16_F, sbyte.MinValue, sbyte.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
@@ -134,22 +134,22 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (byte)Math.Max(0, v1.U8x16_0 - v2.U8x16_0),
-                (byte)Math.Max(0, v1.U8x16_1 - v2.U8x16_1),
-                (byte)Math.Max(0, v1.U8x16_2 - v2.U8x16_2),
-                (byte)Math.Max(0, v1.U8x16_3 - v2.U8x16_3),
-                (byte)Math.Max(0, v1.U8x16_4 - v2.U8x16_4),
-                (byte)Math.Max(0, v1.U8x16_5 - v2.U8x16_5),
-                (byte)Math.Max(0, v1.U8x16_6 - v2.U8x16_6),
-                (byte)Math.Max(0, v1.U8x16_7 - v2.U8x16_7),
-                (byte)Math.Max(0, v1.U8x16_8 - v2.U8x16_8),
-                (byte)Math.Max(0, v1.U8x16_9 - v2.U8x16_9),
-                (byte)Math.Max(0, v1.U8x16_A - v2.U8x16_A),
-                (byte)Math.Max(0, v1.U8x16_B - v2.U8x16_B),
-                (byte)Math.Max(0, v1.U8x16_C - v2.U8x16_C),
-                (byte)Math.Max(0, v1.U8x16_D - v2.U8x16_D),
-                (byte)Math.Max(0, v1.U8x16_E - v2.U8x16_E),
-                (byte)Math.Max(0, v1.U8x16_F - v2.U8x16_F)
+                (byte)Math.Clamp((int)v1.U8x16_0 - (int)v2.U8x16_0, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_1 - (int)v2.U8x16_1, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_2 - (int)v2.U8x16_2, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_3 - (int)v2.U8x16_3, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_4 - (int)v2.U8x16_4, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_5 - (int)v2.U8x16_5, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_6 - (int)v2.U8x16_6, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_7 - (int)v2.U8x16_7, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_8 - (int)v2.U8x16_8, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_9 - (int)v2.U8x16_9, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_A - (int)v2.U8x16_A, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_B - (int)v2.U8x16_B, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_C - (int)v2.U8x16_C, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_D - (int)v2.U8x16_D, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_E - (int)v2.U8x16_E, byte.MinValue, byte.MaxValue),
+                (byte)Math.Clamp((int)v1.U8x16_F - (int)v2.U8x16_F, byte.MinValue, byte.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
@@ -160,14 +160,14 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_0 - v2.I16x8_0)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_1 - v2.I16x8_1)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_2 - v2.I16x8_2)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_3 - v2.I16x8_3)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_4 - v2.I16x8_4)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_5 - v2.I16x8_5)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_6 - v2.I16x8_6)),
-                (short)Math.Min(short.MaxValue, Math.Max(short.MinValue, v1.I16x8_7 - v2.I16x8_7))
+                (short)Math.Clamp((int)v1.I16x8_0 - (int)v2.I16x8_0, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_1 - (int)v2.I16x8_1, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_2 - (int)v2.I16x8_2, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_3 - (int)v2.I16x8_3, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_4 - (int)v2.I16x8_4, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_5 - (int)v2.I16x8_5, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_6 - (int)v2.I16x8_6, short.MinValue, short.MaxValue),
+                (short)Math.Clamp((int)v1.I16x8_7 - (int)v2.I16x8_7, short.MinValue, short.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
@@ -177,14 +177,14 @@ namespace Wacs.Core.Instructions.Numeric
             V128 v2 = context.OpStack.PopV128();
             V128 v1 = context.OpStack.PopV128();
             V128 result = new V128(
-                (short)Math.Max(0, v1.I16x8_0 - v2.I16x8_0),
-                (short)Math.Max(0, v1.I16x8_1 - v2.I16x8_1),
-                (short)Math.Max(0, v1.I16x8_2 - v2.I16x8_2),
-                (short)Math.Max(0, v1.I16x8_3 - v2.I16x8_3),
-                (short)Math.Max(0, v1.I16x8_4 - v2.I16x8_4),
-                (short)Math.Max(0, v1.I16x8_5 - v2.I16x8_5),
-                (short)Math.Max(0, v1.I16x8_6 - v2.I16x8_6),
-                (short)Math.Max(0, v1.I16x8_7 - v2.I16x8_7)
+                (ushort)Math.Clamp((int)v1.U16x8_0 - (int)v2.U16x8_0, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_1 - (int)v2.U16x8_1, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_2 - (int)v2.U16x8_2, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_3 - (int)v2.U16x8_3, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_4 - (int)v2.U16x8_4, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_5 - (int)v2.U16x8_5, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_6 - (int)v2.U16x8_6, ushort.MinValue, ushort.MaxValue),
+                (ushort)Math.Clamp((int)v1.U16x8_7 - (int)v2.U16x8_7, ushort.MinValue, ushort.MaxValue)
             );
             context.OpStack.PushV128(result);
         }
