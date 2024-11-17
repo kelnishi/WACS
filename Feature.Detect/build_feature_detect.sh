e49c3f6b476bbd3427da2aebd1c17edda3aeff4e
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-17 13:02:59 -0800

    Adding build_feature_detect.sh

---
+++ Feature.Detect/build_feature_detect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the parent directory
+PARENT_DIR="./wasm-feature-detect/src/detectors"
+
+OUTPUT_DIR="./generated-wasm"
+
+mkdir -p "$OUTPUT_DIR"
+
+# Iterate over all directories in the parent directory
+for dir in "$PARENT_DIR"/*/; do
+    # Check if it is indeed a directory
+    if [ -d "$dir" ]; then
+        echo "Processing directory: $dir"
+        # Run the node command with the directory as an argument
+        node convert_detector.mjs "$dir" "$OUTPUT_DIR"
+    fi
+done
