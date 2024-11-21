e7af8a2f91a4e844da63446c26175f1d4d2375aa
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-21 15:03:53 -0800

    Expressions more operations

--- Wacs.Core/Instructions/Numeric/I64BinOp.cs
+++ Wacs.Core/Instructions/Numeric/I64BinOp.cs
@@ -14,202 +14,187 @@
 //  * limitations under the License.
 //  */
 
+using System;
 using Wacs.Core.OpCodes;
 using Wacs.Core.Runtime;
 using Wacs.Core.Runtime.Types;
 using Wacs.Core.Types;
+using Wacs.Core.Validation;
 
 namespace Wacs.Core.Instructions.Numeric
 {
-    public partial class NumericInst
+    public abstract class InstI64BinOp : InstructionBase, IConstOpInstruction
     {
         // @Spec 3.3.1.3. i.binop
-        public static readonly NumericInst I64Add = new(OpCode.I64Add, ExecuteI64Add,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
+        public static readonly InstI64BinOp I64Add = new Signed(OpCode.I64Add, ExecuteI64Add,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
 
-        public static readonly NumericInst I64Sub = new(OpCode.I64Sub, ExecuteI64Sub,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
+        public static readonly InstI64BinOp I64Sub = new Signed(OpCode.I64Sub, ExecuteI64Sub,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
 
-        public static readonly NumericInst I64Mul = new(OpCode.I64Mul, ExecuteI64Mul,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
+        public static readonly InstI64BinOp I64Mul = new Signed(OpCode.I64Mul, ExecuteI64Mul,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64), isConst: true);
 
-        public static readonly NumericInst I64DivS = new(OpCode.I64DivS, ExecuteI64DivS,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64DivS = new Signed(OpCode.I64DivS, ExecuteI64DivS,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64DivU = new(OpCode.I64DivU, ExecuteI64DivU,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64DivU = new Unsigned(OpCode.I64DivU, ExecuteI64DivU,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64RemS = new(OpCode.I64RemS, ExecuteI64RemS,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64RemS = new Signed(OpCode.I64RemS, ExecuteI64RemS,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64RemU = new(OpCode.I64RemU, ExecuteI64RemU,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64RemU = new Unsigned(OpCode.I64RemU, ExecuteI64RemU,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64And = new(OpCode.I64And, ExecuteI64And,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64And = new Unsigned(OpCode.I64And, ExecuteI64And,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64Or = new(OpCode.I64Or, ExecuteI64Or,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64Or = new Unsigned(OpCode.I64Or, ExecuteI64Or,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64Xor = new(OpCode.I64Xor, ExecuteI64Xor,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64Xor = new Unsigned(OpCode.I64Xor, ExecuteI64Xor,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64Shl = new(OpCode.I64Shl, ExecuteI64Shl,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64Shl = new Mixed(OpCode.I64Shl, ExecuteI64Shl,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64ShrS = new(OpCode.I64ShrS, ExecuteI64ShrS,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64ShrS = new Signed(OpCode.I64ShrS, ExecuteI64ShrS,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64ShrU = new(OpCode.I64ShrU, ExecuteI64ShrU,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64ShrU = new Mixed(OpCode.I64ShrU, ExecuteI64ShrU,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64Rotl = new(OpCode.I64Rotl, ExecuteI64Rotl,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64Rotl = new Mixed(OpCode.I64Rotl, ExecuteI64Rotl,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        public static readonly NumericInst I64Rotr = new(OpCode.I64Rotr, ExecuteI64Rotr,
-            ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
+        public static readonly InstI64BinOp I64Rotr = new Mixed(OpCode.I64Rotr, ExecuteI64Rotr,
+            NumericInst.ValidateOperands(pop1: ValType.I64, pop2: ValType.I64, push: ValType.I64));
 
-        private static void ExecuteI64Add(ExecContext context)
+        public override ByteCode Op { get; }
+        private readonly NumericInst.ValidationDelegate _validate;
+        public bool IsConstant { get; }
+
+        private InstI64BinOp(ByteCode op, NumericInst.ValidationDelegate validate, bool isConst = false)
         {
-            long i2 = context.OpStack.PopI64();
-            long i1 = context.OpStack.PopI64();
-            long result = i1 + i2;
-            context.OpStack.PushI64(result);
+            Op = op;
+            _validate = validate;
+            IsConstant = isConst;
         }
 
-        private static void ExecuteI64Sub(ExecContext context)
+        private class Signed : InstI64BinOp
         {
-            long i2 = context.OpStack.PopI64();
-            long i1 = context.OpStack.PopI64();
-            long result = i1 - i2;
-            context.OpStack.PushI64(result);
+            private Func<long,long,long> _execute;
+            public Signed(ByteCode op, Func<long,long,long> execute, NumericInst.ValidationDelegate validate,
+                bool isConst = false) : base(op, validate, isConst) => _execute = execute;
+
+            public override int Execute(ExecContext context)
+            {
+                long i2 = context.OpStack.PopI64();
+                long i1 = context.OpStack.PopI64();
+                long result = _execute(i1, i2);
+                context.OpStack.PushI64(result);
+                return 1;
+            }
         }
 
-        private static void ExecuteI64Mul(ExecContext context)
+        private class Unsigned : InstI64BinOp
         {
-            long i2 = context.OpStack.PopI64();
-            long i1 = context.OpStack.PopI64();
-            long result = unchecked(i1 * i2);
-            context.OpStack.PushI64(result);
-        }
+            private Func<ulong,ulong,ulong> _execute;
+            public Unsigned(ByteCode op, Func<ulong,ulong,ulong> execute, NumericInst.ValidationDelegate validate,
+                bool isConst = false) : base(op, validate, isConst) => _execute = execute;
 
-        private static void ExecuteI64DivS(ExecContext context)
+            public override int Execute(ExecContext context)
+            {
+                ulong i2 = context.OpStack.PopU64();
+                ulong i1 = context.OpStack.PopU64();
+                ulong result = _execute(i1, i2);
+                context.OpStack.PushU64(result);
+                return 1;
+            }
+        }
+        
+        private class Mixed : InstI64BinOp
         {
-            long j2 = context.OpStack.PopI64();
-            long j1 = context.OpStack.PopI64();
+            private Func<ulong,long,ulong> _execute;
+            public Mixed(ByteCode op, Func<ulong,long,ulong> execute, NumericInst.ValidationDelegate validate,
+                bool isConst = false) : base(op, validate, isConst) => _execute = execute;
+
+            public override int Execute(ExecContext context)
+            {
+                long i2 = context.OpStack.PopI64();
+                ulong i1 = context.OpStack.PopU64();
+                ulong result = _execute(i1, i2);
+                context.OpStack.PushU64(result);
+                return 1;
+            }
+        }
+        
+        public override void Validate(IWasmValidationContext context) => _validate(context);
+        
+        private static long ExecuteI64Add(long i1, long i2) => i1 + i2;
+
+        private static long ExecuteI64Sub(long i1, long i2) => i1 - i2;
+
+        private static long ExecuteI64Mul(long i1, long i2) => unchecked(i1 * i2);
+
+        private static long ExecuteI64DivS(long j1, long j2)
+        {
             if (j2 == 0)
                 throw new TrapException("Cannot divide by zero");
             if (j2 == -1 && j1 == long.MinValue)
                 throw new TrapException("Operation results in arithmetic overflow");
-            long quotient = j1 / j2;
-            context.OpStack.PushI64(quotient);
+            return j1 / j2;
         }
 
-        private static void ExecuteI64DivU(ExecContext context)
+        private static ulong ExecuteI64DivU(ulong j1, ulong j2)
         {
-            ulong j2 = context.OpStack.PopU64();
-            ulong j1 = context.OpStack.PopU64();
             if (j2 == 0)
                 throw new TrapException("Cannot divide by zero");
-            ulong quotient = j1 / j2;
-            context.OpStack.PushI64((long)quotient);
+            return j1 / j2;
         }
 
-        private static void ExecuteI64RemS(ExecContext context)
+        private static long ExecuteI64RemS(long j1, long j2)
         {
-            long j2 = context.OpStack.PopI64();
-            long j1 = context.OpStack.PopI64();
             if (j2 == 0)
                 throw new TrapException("Cannot divide by zero");
-            
+
             //Special case for arithmetic overflow
-            if (j2 == -1 && j1 == long.MinValue)
-            {
-                context.OpStack.PushI64(0);
-            }
-            else
-            {
-                long remainder = j1 % j2;
-                context.OpStack.PushI64(remainder);
-            }
+            return j2 == -1 && j1 == long.MinValue ? 0 : j1 % j2;
         }
 
-        private static void ExecuteI64RemU(ExecContext context)
+        private static ulong ExecuteI64RemU(ulong j1, ulong j2)
         {
-            ulong j2 = context.OpStack.PopU64();
-            ulong j1 = context.OpStack.PopU64();
             if (j2 == 0)
                 throw new TrapException("Cannot divide by zero");
-            ulong remainder = j1 % j2;
-            context.OpStack.PushI64((long)remainder);
+            return j1 % j2;
         }
 
-        private static void ExecuteI64And(ExecContext context)
-        {
-            ulong i2 = context.OpStack.PopU64();
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = i1 & i2;
-            context.OpStack.PushI64((long)result);
-        }
+        private static ulong ExecuteI64And(ulong i1, ulong i2) => i1 & i2;
 
-        private static void ExecuteI64Or(ExecContext context)
-        {
-            ulong i2 = context.OpStack.PopU64();
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = i1 | i2;
-            context.OpStack.PushI64((long)result);
-        }
+        private static ulong ExecuteI64Or(ulong i1, ulong i2) => i1 | i2;
 
-        private static void ExecuteI64Xor(ExecContext context)
-        {
-            ulong i2 = context.OpStack.PopU64();
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = i1 ^ i2;
-            context.OpStack.PushI64((long)result);
-        }
+        private static ulong ExecuteI64Xor(ulong i1, ulong i2) => i1 ^ i2;
 
-        private static void ExecuteI64Shl(ExecContext context)
-        {
-            int k = (int)context.OpStack.PopI64() & 0x3F;
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = i1 << k;
-            context.OpStack.PushI64((long)result);
-        }
+        private static ulong ExecuteI64Shl(ulong i1, long i2) => i1 << ((int)i2 & 0x3F);
 
-        private static void ExecuteI64ShrS(ExecContext context)
-        {
-            int k = (int)context.OpStack.PopI64() & 0x3F;
-            long i1 = context.OpStack.PopI64();
-            long result = i1 >> k;
-            context.OpStack.PushI64(result);
-        }
+        private static long ExecuteI64ShrS(long i1, long i2) => i1 >> ((int)i2 & 0x3F);
 
-        private static void ExecuteI64ShrU(ExecContext context)
-        {
-            int k = (int)context.OpStack.PopI64() & 0x3F;
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = i1 >> k;
-            context.OpStack.PushI64((long)result);
-        }
+        private static ulong ExecuteI64ShrU(ulong i1, long i2) => i1 >> ((int)i2 & 0x3F);
 
-        private static void ExecuteI64Rotl(ExecContext context)
+        private static ulong ExecuteI64Rotl(ulong i1, long i2)
         {
-            int k = (int)context.OpStack.PopI64() & 0x3F;
-            ulong i1 = context.OpStack.PopU64();
-
+            int k = (int)i2 & 0x3F;
             ulong result = i1 << k;
             if (k != 0)
                 result |= i1 >> (64 - k);
-
-            context.OpStack.PushI64((long)result);
+            return result;
         }
 
-        private static void ExecuteI64Rotr(ExecContext context)
+        private static ulong ExecuteI64Rotr(ulong i1, long i2)
         {
-            int k = (int)context.OpStack.PopI64() & 0x3F;
-            ulong i1 = context.OpStack.PopU64();
-            ulong result = (i1 >> k) | (i1 << (64 - k));
-            context.OpStack.PushI64((long)result);
+            int k = (int)i2 & 0x3F;
+            return (i1 >> k) | (i1 << (64 - k));
         }
     }
 }
