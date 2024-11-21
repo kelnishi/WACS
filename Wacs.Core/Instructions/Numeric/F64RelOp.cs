678bfa20b6c880541722881dc36e8a22f7f2613a
Kelvin Nishikawa <kelnishi@gmail.com>
2024-11-21 13:50:03 -0800

    Expressions more operations

--- Wacs.Core/Instructions/Numeric/F64RelOp.cs
+++ Wacs.Core/Instructions/Numeric/F64RelOp.cs
@@ -14,99 +14,71 @@
 //  * limitations under the License.
 //  */
 
+using System;
 using Wacs.Core.OpCodes;
 using Wacs.Core.Runtime;
 using Wacs.Core.Types;
+using Wacs.Core.Validation;
 
 namespace Wacs.Core.Instructions.Numeric
 {
-    public partial class NumericInst
+    public class InstF64RelOp : InstructionBase
     {
-        public static readonly NumericInst F64Eq = new(OpCode.F64Eq, ExecuteF64Eq,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Eq = new(OpCode.F64Eq, ExecuteF64Eq,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-        public static readonly NumericInst F64Ne = new(OpCode.F64Ne, ExecuteF64Ne,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Ne = new(OpCode.F64Ne, ExecuteF64Ne,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-        public static readonly NumericInst F64Lt = new(OpCode.F64Lt, ExecuteF64Lt,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Lt = new(OpCode.F64Lt, ExecuteF64Lt,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-        public static readonly NumericInst F64Gt = new(OpCode.F64Gt, ExecuteF64Gt,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Gt = new(OpCode.F64Gt, ExecuteF64Gt,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-        public static readonly NumericInst F64Le = new(OpCode.F64Le, ExecuteF64Le,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Le = new(OpCode.F64Le, ExecuteF64Le,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-        public static readonly NumericInst F64Ge = new(OpCode.F64Ge, ExecuteF64Ge,
-            ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
+        public static readonly InstF64RelOp F64Ge = new(OpCode.F64Ge, ExecuteF64Ge,
+            NumericInst.ValidateOperands(pop1: ValType.F64, pop2: ValType.F64, push: ValType.I32));
 
-
-        private static void ExecuteF64Eq(ExecContext context)
+        
+        public override ByteCode Op { get; }
+        
+        private readonly NumericInst.ValidationDelegate _validate;
+        private Func<double,double,int> _execute;
+        
+        private InstF64RelOp(ByteCode op, Func<double,double,int> execute, NumericInst.ValidationDelegate validate)
+        {
+            Op = op;
+            _execute = execute;
+            _validate = validate;
+        }
+        
+        public override void Validate(IWasmValidationContext context) => _validate(context);
+        public override int Execute(ExecContext context)
         {
             double i2 = context.OpStack.PopF64();
             double i1 = context.OpStack.PopF64();
-
-            int result = i1 == i2 ? 1 : 0;
-            
+            int result = _execute(i1, i2);
             context.OpStack.PushI32(result);
+            return 1;
         }
 
-        private static void ExecuteF64Ne(ExecContext context)
-        {
-            double i2 = context.OpStack.PopF64();
-            double i1 = context.OpStack.PopF64();
+        private static int ExecuteF64Eq(double i1, double i2) => i1 == i2 ? 1 : 0;
 
-            int result = i1 != i2 ? 1 : 0;
+        private static int ExecuteF64Ne(double i1, double i2) => i1 != i2 ? 1 : 0;
 
-            context.OpStack.PushI32(result);
-        }
+        private static int ExecuteF64Lt(double i1, double i2) => 
+            double.IsNaN(i1) || double.IsNaN(i2) ? 0 : i1 < i2 ? 1 : 0;
 
-        private static void ExecuteF64Lt(ExecContext context)
-        {
-            double i2 = context.OpStack.PopF64();
-            double i1 = context.OpStack.PopF64();
+        private static int ExecuteF64Gt(double i1, double i2) => 
+            double.IsNaN(i1) || double.IsNaN(i2) ? 0 : i1 > i2 ? 1 : 0;
 
-            int result = i1 < i2 ? 1 : 0;
-            if (double.IsNaN(i1) || double.IsNaN(i2))
-                result = 0;
+        private static int ExecuteF64Le(double i1, double i2) => 
+            double.IsNaN(i1) || double.IsNaN(i2) ? 0 : i1 <= i2 ? 1 : 0;
 
-            context.OpStack.PushI32(result);
-        }
-
-        private static void ExecuteF64Gt(ExecContext context)
-        {
-            double i2 = context.OpStack.PopF64();
-            double i1 = context.OpStack.PopF64();
-
-            int result = i1 > i2 ? 1 : 0;
-            if (double.IsNaN(i1) || double.IsNaN(i2))
-                result = 0;
-
-            context.OpStack.PushI32(result);
-        }
-
-        private static void ExecuteF64Le(ExecContext context)
-        {
-            double i2 = context.OpStack.PopF64();
-            double i1 = context.OpStack.PopF64();
-
-            int result = i1 <= i2 ? 1 : 0;
-            if (double.IsNaN(i1) || double.IsNaN(i2))
-                result = 0;
-
-            context.OpStack.PushI32(result);
-        }
-
-        private static void ExecuteF64Ge(ExecContext context)
-        {
-            double i2 = context.OpStack.PopF64();
-            double i1 = context.OpStack.PopF64();
-
-            int result = i1 >= i2 ? 1 : 0;
-            if (double.IsNaN(i1) || double.IsNaN(i2))
-                result = 0;
-
-            context.OpStack.PushI32(result);
-        }
+        private static int ExecuteF64Ge(double i1, double i2) => 
+            double.IsNaN(i1) || double.IsNaN(i2) ? 0 : i1 >= i2 ? 1 : 0;
     }
 }
