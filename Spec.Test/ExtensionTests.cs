0095b68bb83859c76c3063df821a09e20009a9ac
Kelvin Nishikawa <kelnishi@gmail.com>
2024-12-03 20:17:32 -0800

    Adding Transpiled test for tailcall

--- Spec.Test/ExtensionTests.cs
+++ Spec.Test/ExtensionTests.cs
@@ -28,6 +28,25 @@ namespace Spec.Test
         public void TailCallFactorial()
         {
             var runtime = new WasmRuntime();
+            runtime.TranspileModules = false;
+            
+            using var fileStream = new FileStream("../../../engine/tailcalls.wasm", FileMode.Open);
+            var module = BinaryModuleParser.ParseWasm(fileStream);
+            var moduleInst = runtime.InstantiateModule(module);
+            runtime.RegisterModule("tailcalls", moduleInst);
+
+            var fa = runtime.GetExportedFunction(("tailcalls", "factorial"));
+            var invoker = runtime.CreateInvoker<Func<long, long>>(fa);
+
+            Assert.Equal(479_001_600, invoker(12));
+            Assert.Equal(2_432_902_008_176_640_000 , invoker(20));
+        }
+        
+        [Fact]
+        public void TailCallFactorialTranspiled()
+        {
+            var runtime = new WasmRuntime();
+            runtime.TranspileModules = true;
             
             using var fileStream = new FileStream("../../../engine/tailcalls.wasm", FileMode.Open);
             var module = BinaryModuleParser.ParseWasm(fileStream);
