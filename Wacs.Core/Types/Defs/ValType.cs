af7210a044fc99da738a805de00d27b99ea9e45b
Kelvin Nishikawa <kelnishi@gmail.com>
2024-12-10 13:38:29 -0800

    removing incorrect type validation

--- Wacs.Core/Types/Defs/ValType.cs
+++ Wacs.Core/Types/Defs/ValType.cs
@@ -310,15 +310,6 @@ namespace Wacs.Core.Types.Defs
             
             if (left.IsRefType() && right.IsRefType())
             {
-                //Special case these for validation
-                left = left switch
-                {
-                    ValType.None => ValType.NoneNN,
-                    ValType.NoFunc => ValType.NoFuncNN,
-                    ValType.NoExtern => ValType.NoExternNN,
-                    _ => left
-                };
-
                 //Check the HeapType
                 //Non-nullable cannot receive nullable
                 return left.IsSubType(right, types) && (!left.IsNullable() || right.IsNullable());
